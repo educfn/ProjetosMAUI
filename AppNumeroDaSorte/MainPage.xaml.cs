9dc34ad9739862e469d4c3dd593e97121c5636d2
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-08-25 14:56:53 -0300

    25. 13 - Tela Inicial - Parte 4

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -6,4 +6,10 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnGenerateLuckNumber(object sender, EventArgs e)
+	{
+		NameApp.IsVisible = false;
+		ContainerLuckNumbers.IsVisible = true;
+	}
 }
