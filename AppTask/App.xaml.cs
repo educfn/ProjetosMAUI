541dc0add4337feea830588c0d8aadc211fcf188
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-09-12 22:20:43 -0300

    157. 11 - AddEditTaskPage - Border no Entry, Editor e DatePicker

--- AppTask/App.xaml.cs
+++ AppTask/App.xaml.cs
@@ -17,6 +17,12 @@ namespace AppTask
 
         private void CustomHandler()
         {
+            EntryNoBorder();
+            DatePickerNoBorder();
+        }
+
+        private static void EntryNoBorder()
+        {
             Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
             {
 #if ANDROID
@@ -31,5 +37,24 @@ namespace AppTask
 #endif
             });
         }
+
+        private static void DatePickerNoBorder()
+        {
+            Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+#if ANDROID
+                //ANDROID
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+#elif IOS || MACCATALYST
+                //iOS || MACCATALYST
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                //WINDOWS - Não funciona 100%
+                handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+            });
+        }
+
+
     }
 }
