b5288d6d97120a447bc33c2e5e8a55b86ab67d8c
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-09-17 20:48:46 -0300

    162. 16 - Configurar DbContext - Parte 2

--- AppTask.Database/AppTaskContext.cs
+++ AppTask.Database/AppTaskContext.cs
@@ -12,5 +12,14 @@ namespace AppTask.Database
     {
         public DbSet<TaskModel> Tasks { get; set; }
         public DbSet<SubTaskModel> SubTasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "apptask.db");
+
+            optionsBuilder.UseSqlite($"Filename={databasePath}");
+        }
     }
 }
