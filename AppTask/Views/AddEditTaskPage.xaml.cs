75bbd52711862c1f05e63e7b134be5b0f9cacfe0
Eduardo CFN <eduardo.cardoso10@gmail.com>
2024-09-28 17:01:24 -0300

    180. 32 - AddEditTasPage - Salvar - Parte 1 - Obter os dados

--- AppTask/Views/AddEditTaskPage.xaml.cs
+++ AppTask/Views/AddEditTaskPage.xaml.cs
@@ -15,10 +15,32 @@ public partial class AddEditTaskPage : ContentPage
 
     private void CloseModal(object sender, EventArgs e)
 	{
+		//Obter os dados
+		GetDataFromForm();
+		//Validar os dados
+
+		//Salvar os dados
+
+		//Fechar a tela
+
+		//Solicitar a atualização da listagem da tela anterior.
 		Navigation.PopModalAsync();
 	}
 
-	private void SaveData(object sender, EventArgs e)
+	private void GetDataFromForm()
+	{
+		_task.Name = Entry_TaskName.Text;
+		_task.Name = Editor_TaskDescription.Text;
+		_task.PrevisionDate = DatePicker_TaskDate.Date;
+		_task.PrevisionDate = _task.PrevisionDate.AddHours(23);
+		_task.PrevisionDate = _task.PrevisionDate.AddMinutes(59);
+		_task.PrevisionDate = _task.PrevisionDate.AddSeconds(59);
+
+		_task.Created = DateTime.Now;
+		_task.IsCompleted = false;
+	}
+
+    private void SaveData(object sender, EventArgs e)
 	{
 		Navigation.PopModalAsync();
 	}
